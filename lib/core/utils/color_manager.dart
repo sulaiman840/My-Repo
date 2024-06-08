a22e9b3df911014336ada78a365db12338e8591a
ftmalhlou <117488369+ftmalhlou@users.noreply.github.com>
2024-06-09 02:32:56 +0300

    adding some folders

---
+++ lib/core/utils/color_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColorManager
+{
+  //basic color
+  static const bc0 = Color(0xFFF8F9FB); 
+  static const bc1 = Color(0xFFE7ECF0); 
+  static const bc2 = Color(0xFFC5CDD2);
+  static const bc3 = Color(0xFF606D76); 
+  static const bc4 = Color(0xFF354349); 
+  static const bc5 = Color(0xFF1B262E); 
+
+
+  static const bluelight = Color(0xFF2CA1DA); 
+  static const blue = Color(0xFF0A70B7);
+
+  static const orange = Color(0xFFF9B023); 
+  static const saffron = Color(0xFFFFC83A); 
+ 
+  
+}
+
