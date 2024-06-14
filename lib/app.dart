8a752216dcfafd968cc739dbc2736f9cbab493fb
ftmalhlou <117488369+ftmalhlou@users.noreply.github.com>
2024-06-14 11:10:08 +0300

    adding 2 classes

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'screens/Home/ware_house_app.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home:WareHouseMApp() ,
+    );
   }
 }
