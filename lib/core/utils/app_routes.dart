34af542b70798f8fc36a41e15f4f4e309565f318
ftmalhlou <117488369+ftmalhlou@users.noreply.github.com>
2024-06-26 01:08:29 +0300

    login and reg screens

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,8 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'package:project2/screens/login/login_screen.dart';
 
 import '../../screens/Home/manger_home.dart';
 import '../../screens/Manager_Screens/profile_screen.dart';
+import '../../screens/register/register_screen.dart';
 
 
 class AppRouter {
@@ -10,8 +12,12 @@ class AppRouter {
   static const String profile = '/profile';
 
   static final routes = <String, WidgetBuilder>{
+    LoginScreen.id:(context) =>LoginScreen(),
+    RegisterScreen.id:(context) =>LoginScreen(),
     home: (context) => ManagerHome(),
     profile: (context) => ProfileScreen(),
+
+      
 
   };
 }
