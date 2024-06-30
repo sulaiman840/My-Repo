ad077833a30c41d604e4c3ecaae8322483a631a6
sulaiman840 <150594965+sulaiman840@users.noreply.github.com>
2024-06-30 22:10:45 +0300

    Hello

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: "AIzaSyByFyhWeTuXTxC2OVT3w0-OmI7q-WHx3hc",
+  authDomain: "warehouse-management-d8a87.firebaseapp.com",
+  projectId: "warehouse-management-d8a87",
+  storageBucket: "warehouse-management-d8a87.appspot.com",
+  messagingSenderId: "193004774523",
+  appId: "1:193004774523:web:4b3b459ea4d55f02a06ede",
+  measurementId: "G-WBG1QY7VP6",
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+// main.dart
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'firebase_messaging_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseOptions);
+  await FirebaseMessagingService.initialize();
   runApp(const MyApp());
-
 }
-
-
