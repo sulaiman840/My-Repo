a1993906dfc556c7a6cf33c50bcc83d41a893da3
ftmalhlou <117488369+ftmalhlou@users.noreply.github.com>
2024-07-05 12:59:20 +0300

    add type feature ui

---
+++ lib/screens/Manager_Screens/warehouse/warehouse.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project2/core/utils/app_manager.dart';
+import 'package:project2/core/utils/color_manager.dart';
+import 'package:project2/core/utils/style_manager.dart';
+
+
+import 'widget/circular_icon_widget.dart';
+import 'widget/elevated_btn_widget.dart';
+import 'widget/type_item_widget.dart';
+
+class WareHouse extends StatelessWidget {
+  const WareHouse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.symmetric(horizontal: AppPadding.p16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+       
+          //create new type
+      
+          elevatedbtn(
+            icon: circleIconWidget(icon: Icons.add,
+            backgroundColor: ColorManager.orange, color: ColorManager.bc0),
+            text: 'Add New Type',
+            style: StyleManager.labelMedium(color: ColorManager.bc4),
+            onPressed: ()
+            {
+              //الربط هنا
+            },
+         
+          ),
+           
+           const SizedBox(height: AppSize.s20,),
+          // list of type item
+          Expanded(
+            child:ListView.separated(
+            itemBuilder:(context,index)=> TypeItemWidget(
+          //الضغط على العنصر
+          onTap: ()
+          {
+            //الربط هنا
+          },
+        ),
+       
+            separatorBuilder: (context,index)=>const SizedBox(
+              height: AppSize.s20,
+            ), 
+            itemCount: 5)),
+        
+      
+          ],
+      ),
+    );
+  }
+}
